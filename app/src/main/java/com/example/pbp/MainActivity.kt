9fcc78fa55081fa478622a91bca313d096b505bb
JollyFrankle <jollyfrankle3@gmail.com>
2022-09-04 16:25:09 +0700

    Rev 20220904-1

--- app/src/main/java/com/example/pbp/MainActivity.kt
+++ app/src/main/java/com/example/pbp/MainActivity.kt
@@ -9,5 +9,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /// PUJI TUHAN AKHIRNYA BISA
+
+        val actionbar = supportActionBar
+
+        // disable actionbar
+        actionbar!!.hide()
+
+        // set statusbar color
+        window.statusBarColor = resources.getColor(R.color.color_secondary_variant, theme)
+
+        // set actionbar title
+        actionbar!!.title = "Home"
+
+        // set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
     }
 }
